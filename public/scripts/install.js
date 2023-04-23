4e86d438e731043c4f8d2135e84a4febd1d28731
Soyab Nandhla <admin@soyabnandhla.me>
2023-04-23 15:39:42 +0100

    update install.js

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -1,31 +1,37 @@
-"use strict";
-
-let deferredInstallPrompt = null;
 const installButton = document.getElementById("Install");
-installButton.addEventListener("click", installPWA);
-
-window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
 
 function saveBeforeInstallPromptEvent(evt) {
   deferredInstallPrompt = evt;
   installButton.removeAttribute("hidden");
 }
 
-function installPWA(evt) {
+function logAppInstalled(evt) {
+  console.log("Weather App was installed.", evt);
+}
+
+function handleEvent(evt) {
+  switch (evt.type) {
+    case "beforeinstallprompt":
+      saveBeforeInstallPromptEvent(evt);
+      break;
+    case "appinstalled":
+      logAppInstalled(evt);
+      break;
+  }
+}
+
+async function installPWA(evt) {
   deferredInstallPrompt.prompt();
   evt.srcElement.setAttribute("hidden", false);
-  deferredInstallPrompt.userChoice.then(choice => {
-    if (choice.outcome === "accepted") {
-      console.log("User accepted the A2HS prompt", choice);
-    } else {
-      console.log("User dismissed the A2HS prompt", choice);
-    }
-    deferredInstallPrompt = null;
-  });
+  const choice = await deferredInstallPrompt.userChoice;
+  if (choice.outcome === "accepted") {
+    console.log("User accepted the A2HS prompt", choice);
+  } else {
+    console.log("User dismissed the A2HS prompt", choice);
+  }
+  deferredInstallPrompt = null;
 }
 
-window.addEventListener("appinstalled", logAppInstalled);
-
-function logAppInstalled(evt) {
-  console.log("Pizza Delivery App was installed.", evt);
-}
+installButton.addEventListener("click", installPWA);
+window.addEventListener("beforeinstallprompt", handleEvent);
+window.addEventListener("appinstalled", handleEvent);
